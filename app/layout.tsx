054782f5bfa5baf1fcc67da315786efc29365313
Yoga Permana <yoga@brandztory.id>
2023-09-05 16:33:11 +0700

    feat: formatter and linter setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,25 @@
-import { ThemeProvider } from "@/components/sites/theme-provider";
-import "./globals.css";
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { ThemeProvider } from "@/components/sites/theme-provider"
+
+import "./globals.css"
+
+import type { Metadata } from "next"
+import localFont from "next/font/local"
 
 const fontSans = localFont({
   src: "./0xProto.woff2",
   display: "swap",
   variable: "--font-sans",
-});
+})
 
 export const metadata: Metadata = {
   title: "Yoga Permana",
   description: "Yoga Permana's Personal Website",
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" className={`${fontSans.variable} font-sans`}>
@@ -27,5 +29,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import { ThemeToggle } from "@/components/sites/theme-toggle";
-import Image from "next/image";
+import Image from "next/image"
+
+import { ThemeToggle } from "@/components/sites/theme-toggle"
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen">
+    <main className="min-h-screen w-full">
       <div className="container mx-auto flex flex-col space-y-4">
-        <div className="w-full py-4 flex justify-between">
+        <div className="flex w-full justify-between px-4 py-4">
           <div>dy</div>
           <ThemeToggle />
         </div>
         <h1 className="text-center">Hello World</h1>
       </div>
     </main>
-  );
+  )
 }
